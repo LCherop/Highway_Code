4f94317bebfb50e20143f1f837a35b3751bb257a
Laura Cherop <77297998+LCherop@users.noreply.github.com>
2021-04-26 13:09:58 +0300

    Update alloffensesController.php

--- highway_code/app/Http/Controllers/alloffensesController.php
+++ highway_code/app/Http/Controllers/alloffensesController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use App\Models\offences;
 use Illuminate\Http\Request;
-use App\Models\User;
 class alloffensesController extends Controller
 {
     public function alloffenses(){
